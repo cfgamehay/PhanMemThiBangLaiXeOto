ba83c106415a3216c9f37f6ea78d88aa91f8f4ba
Pnix Jone <ha.truong.04071982@gmail.com>
2026-03-11 20:29:24 +0700

    add middleware admin

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using ApiThiBangLaiXeOto.Data;
+using ApiThiBangLaiXeOto.Helper;
+using ApiThiBangLaiXeOto.Middleware;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -11,8 +13,8 @@ namespace ApiThiBangLaiXeOto.Controllers
     public class UserController : Controller
     {
         private readonly SqlHelper _sql;
-        public UserController(SqlHelper sql) 
-        { 
+        public UserController(SqlHelper sql)
+        {
             _sql = sql;
         }
         [HttpPatch("ChangePassword")]
@@ -29,19 +31,27 @@ namespace ApiThiBangLaiXeOto.Controllers
             return StatusCode(500, new { Success = false, Message = "Có lỗi xảy ra khi đổi mật khẩu." });
         }
         [HttpGet("Profile")]
-        public async Task<IActionResult> Profile()
+        public IActionResult Profile()
         {
-            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
-            if (userIdClaim != null && int.TryParse(userIdClaim.Value, out int userId))
+            try
             {
-                var user = await _sql.GetUserAsync(userId, null);
                 return Ok(new
                 {
                     success = true,
-                    data = user
+                    data = authHelper.GetUser(User, _sql).Result,
                 });
             }
-            return BadRequest(new { message = "Không tải được thông tin" });
+            catch
+            {
+                return BadRequest(new { message = "Không tải được thông tin" });
+            }
+        }
+        [AdminOnly]
+        [HttpGet("Admin")]
+        public IActionResult Admin()
+        {
+            return Ok(new { message = "Đây là trang Admin" });
         }
     }
 }
+
---
+++ Helper/authHelper.cs
@@ -0,0 +1,20 @@
+﻿using ApiThiBangLaiXeOto.Data;
+using ApiThiBangLaiXeOto.DTOs;
+using System.Security.Claims;
+
+namespace ApiThiBangLaiXeOto.Helper
+{
+    public static class authHelper
+    {
+        public static async Task<UserDTO> GetUser(ClaimsPrincipal User, SqlHelper _sql)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            if (userIdClaim != null && int.TryParse(userIdClaim.Value, out int userId))
+            {
+                UserDTO user = await _sql.GetUserAsync(userId, null);
+                return user;
+            }
+            return null;
+        }
+    }
+}
